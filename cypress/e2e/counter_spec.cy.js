5143073c5065b90bb8d319f7a5b1f07ce08e5826
sarah <sarah.iglla@gmail.com>
2024-10-30 20:59:53 +0100

    creation projet de test javascript avec cypress

---
+++ cypress/e2e/counter_spec.cy.js
@@ -0,0 +1,29 @@
+describe('counter test', () => {
+  beforeEach(()=>{
+    cy.visit('http://localhost:5173/')
+  })
+  it('passes', () => {
+    cy.get('#counter')
+  })
+  it('affiche un compteur intial à 0', () => {
+    cy.get('#counter').should("have.text", "count is 0")
+  })
+  it('affiche un compteur à 1', () => {
+    cy.get('#counter').click()
+    cy.get('#counter').should('have.text', 'count is 1')
+  })
+  it('affiche un compteur à 3', () => {
+    cy.get('#counter').click().click().click()
+    cy.get('#counter').should('have.text', 'count is 3')
+  })
+  it('affiche la documentation vite', () => {
+    cy.get('.logo.vanilla').should('be.visible')
+  })
+  it('affiche la documentation vite', () => {
+    cy.get('.logo')
+    .should('be.visible')
+    .and('have.attr', 'src')
+    .and('include', 'vite.svg');
+  })
+
+})
